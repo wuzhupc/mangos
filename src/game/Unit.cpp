a6a67d27b1d386f6d65e89a9c252b074725b5358
wuzhupc <wuzhupc@gmail.com>
2012-06-06 16:30:17 +0800

    整合mangosr2和scriptdev2 至2012-06-06 14:00:00
    Signed-off-by: wuzhupc <wuzhupc@gmail.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -1373,6 +1373,7 @@ void Unit::CastSpell(Unit* Victim, SpellEntry const *spellInfo, bool triggered, 
     }
 	
 	//wuzhu start 略过的魔法列表 100903
+	/*
 	const uint32 filterspellidlist[]={26615,26662,27680,28498,35595,45078,46587,47008,55816,61632,
 		61714,61715,62555,64112,64238,65126,68378,72525};
 	int length=sizeof(filterspellidlist)/sizeof(int);
@@ -1382,6 +1383,7 @@ void Unit::CastSpell(Unit* Victim, SpellEntry const *spellInfo, bool triggered, 
 		sLog.outError("WUZHU CastSpell: %d spell by caster: %s if filter!!!!",filterspellidlist[i],GetObjectGuid().GetString().c_str());
 		return;
 	}
+	*/
 	//wuzhu end
 
     if (castItem)
@@ -1592,18 +1594,11 @@ void Unit::CalculateSpellDamage(DamageInfo* damageInfo, float DamageMultiplier)
                                                 damageInfo->target->GetMeleeCritDamageReduction(reduction_affected_damage) :
                                                 damageInfo->target->GetRangedCritDamageReduction(reduction_affected_damage);
 
-                uint32 damageRegularReduction = damageInfo->target->GetSpellDamageReduction(reduction_affected_damage);
-                damageInfo->damage -= std::max(damageCritReduction,damageRegularReduction);
+                damageInfo->damage -= damageCritReduction;
             }
             else
             {
-                damageInfo->HitInfo &=  ~SPELL_HIT_TYPE_CRIT;
-
-                // Resilience - reduce regular damage (full or reduced)
-                uint32 reduction_affected_damage = sWorld.getConfig(CONFIG_BOOL_RESILENCE_ALTERNATIVE_CALCULATION) ?
-                                                   damageInfo->damage :
-                                                   CalcNotIgnoreDamageReduction(damageInfo);
-                damageInfo->damage -= damageInfo->target->GetSpellDamageReduction(reduction_affected_damage);
+                damageInfo->HitInfo &= ~SPELL_HIT_TYPE_CRIT;
             }
             break;
         }
@@ -1628,19 +1623,11 @@ void Unit::CalculateSpellDamage(DamageInfo* damageInfo, float DamageMultiplier)
                                                    damageInfo->damage :
                                                    CalcNotIgnoreDamageReduction(damageInfo);
 
-                uint32 damageCritReduction    = damageInfo->target->GetSpellCritDamageReduction(reduction_affected_damage);
-                uint32 damageRegularReduction = damageInfo->target->GetSpellDamageReduction(reduction_affected_damage);
-                damageInfo->damage -= std::max(damageCritReduction,damageRegularReduction);
+                damageInfo->damage -= damageInfo->target->GetSpellCritDamageReduction(reduction_affected_damage);
             }
             else
             {
                 damageInfo->HitInfo &= ~SPELL_HIT_TYPE_CRIT;
-
-                // Resilience - reduce regular damage (full or reduced)
-                uint32 reduction_affected_damage = sWorld.getConfig(CONFIG_BOOL_RESILENCE_ALTERNATIVE_CALCULATION) ?
-                                                   damageInfo->damage :
-                                                   CalcNotIgnoreDamageReduction(damageInfo);
-                damageInfo->damage -= damageInfo->target->GetSpellDamageReduction(reduction_affected_damage);
             }
             break;
         }
@@ -1648,6 +1635,12 @@ void Unit::CalculateSpellDamage(DamageInfo* damageInfo, float DamageMultiplier)
             sLog.outError("Unit::CalculateSpellDamage unknown damage class by caster: %s, spell %u", GetObjectGuid().GetString().c_str(), damageInfo->m_spellInfo->Id);
             return;
     }
+
+    // Resilience - reduce regular damage (full or reduced)
+    uint32 reduction_affected_damage = sWorld.getConfig(CONFIG_BOOL_RESILENCE_ALTERNATIVE_CALCULATION) ?
+                                       damageInfo->damage :
+                                       CalcNotIgnoreDamageReduction(damageInfo);
+    damageInfo->damage -= damageInfo->target->GetSpellDamageReduction(reduction_affected_damage);
 
     // damage mitigation
     if (damageInfo->damage > 0)
@@ -2983,7 +2976,7 @@ void Unit::AttackerStateUpdate (Unit *pVictim, WeaponAttackType attType, bool ex
     // attack can be redirected to another target
     pVictim = SelectMagnetTarget(pVictim);
 
-    DamageInfo damageInfo = DamageInfo(this, pVictim);
+    DamageInfo damageInfo = DamageInfo(this, pVictim, uint32(0),0);
     damageInfo.attackType       = attType;
 
     CalculateMeleeDamage(&damageInfo);
@@ -6175,8 +6168,7 @@ void Unit::SendSpellNonMeleeDamageLog(DamageInfo *log)
 
 void Unit::SendSpellNonMeleeDamageLog(Unit *target, uint32 SpellID, uint32 Damage, SpellSchoolMask damageSchoolMask, uint32 AbsorbedDamage, uint32 Resist, bool PhysicalDamage, uint32 Blocked, bool CriticalHit)
 {
-    DamageInfo log(this, target, SpellID);
-    log.damage = Damage - AbsorbedDamage - Resist - Blocked;
+    DamageInfo log(this, target, SpellID,(Damage - AbsorbedDamage - Resist - Blocked));
     log.absorb = AbsorbedDamage;
     log.resist = Resist;
     log.physicalLog = PhysicalDamage;
@@ -13739,10 +13731,14 @@ void DamageInfo::Reset(uint32 _damage)
         SpellID = m_spellInfo->Id;
 
     damage        = _damage;
+    baseDamage    = _damage;
     cleanDamage   = 0;
     absorb        = 0;
     resist        = 0;
     blocked       = 0;
+    reduction     = 0;
+    bonusDone     = 0;
+    bonusTaken    = 0;
     unused        = false;
     HitInfo       = HITINFO_NORMALSWING;
     TargetState   = VICTIMSTATE_UNAFFECTED;
